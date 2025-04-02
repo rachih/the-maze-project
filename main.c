61948fde397836bb03e22b1f660310c3c936ffae
rachih <79285353+rachih@users.noreply.github.com>
2025-04-03 01:19:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,85 +1,142 @@
+#include <C:\SDL22GAM\src\include\SDL2/SDL.h>
 #include <stdio.h>
-#include <conio.h>  
-#include <windows.h> 
+#include <math.h>
 
-#define WIDTH 10
-#define HEIGHT 10
+// Window dimensions
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
+// Map dimensions
+#define MAP_WIDTH 8
+#define MAP_HEIGHT 8
 
-#define COLOR_RESET  "\033[0m"
-#define COLOR_WALL   "\033[1;34m"  
-#define COLOR_PATH   "\033[0;37m"  
-#define COLOR_PLAYER "\033[1;32m"  
-
-char maze[HEIGHT][WIDTH] = {
-    "##########",
-    "#        #",
-    "# ###### #",
-    "# #    # #",
-    "# # ## # #",
-    "# # ## # #",
-    "# #    # #",
-    "# ###### #",
-    "#        #",
-    "##########"
+// The 2D map (1 represents walls, 0 represents empty space)
+int map[MAP_WIDTH][MAP_HEIGHT] = {
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 1, 1, 1, 0, 1},
+    {1, 0, 1, 0, 0, 1, 0, 1},
+    {1, 0, 1, 0, 1, 1, 0, 1},
+    {1, 0, 1, 1, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1}
 };
 
-int playerX = 1, playerY = 1;
+// Player position and direction
+float posX = 2.0f, posY = 2.0f;  // Player's position
+float dirX = -1.0f, dirY = 0.0f; // Initial direction vector
+float planeX = 0.0f, planeY = 0.66f; // 2D camera plane
 
-void displayMaze() {
-    system("cls"); 
+// Function to initialize SDL
+int initSDL(SDL_Window **window, SDL_Renderer **renderer) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
 
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            if (i == playerY && j == playerX)
-                printf(COLOR_PLAYER "P " COLOR_RESET);  
-            else if (maze[i][j] == '#')
-                printf(COLOR_WALL "# " COLOR_RESET);  
-            else
-                printf(COLOR_PATH ". " COLOR_RESET);  
+    *window = SDL_CreateWindow("Raycasting Maze", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!*window) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
+    if (!*renderer) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    return 0;
+}
+
+// Function to render the 3D view
+void render3D(SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Clear screen with black
+    SDL_RenderClear(renderer);
+
+    for (int x = 0; x < SCREEN_WIDTH; x++) {
+        float cameraX = 2 * x / (float)SCREEN_WIDTH - 1;
+        float rayDirX = dirX + planeX * cameraX;
+        float rayDirY = dirY + planeY * cameraX;
+
+        int mapX = (int)posX;
+        int mapY = (int)posY;
+        float sideDistX, sideDistY;
+        float deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
+        float deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
+        float perpWallDist;
+        int stepX, stepY, hit = 0, side;
+
+        if (rayDirX < 0) {
+            stepX = -1;
+            sideDistX = (posX - mapX) * deltaDistX;
+        } else {
+            stepX = 1;
+            sideDistX = (mapX + 1.0 - posX) * deltaDistX;
         }
-        printf("\n");
+
+        if (rayDirY < 0) {
+            stepY = -1;
+            sideDistY = (posY - mapY) * deltaDistY;
+        } else {
+            stepY = 1;
+            sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+        }
+
+        while (hit == 0) {
+            if (sideDistX < sideDistY) {
+                sideDistX += deltaDistX;
+                mapX += stepX;
+                side = 0;
+            } else {
+                sideDistY += deltaDistY;
+                mapY += stepY;
+                side = 1;
+            }
+
+            if (mapX >= 0 && mapX < MAP_WIDTH && mapY >= 0 && mapY < MAP_HEIGHT) {
+                if (map[mapX][mapY] > 0) hit = 1;
+            }
+        }
+
+        if (side == 0)
+            perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
+        else
+            perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
+
+        int lineHeight = (int)(SCREEN_HEIGHT / perpWallDist);
+        int drawStart = -lineHeight / 2 + SCREEN_HEIGHT / 2;
+        if (drawStart < 0) drawStart = 0;
+        int drawEnd = lineHeight / 2 + SCREEN_HEIGHT / 2;
+        if (drawEnd >= SCREEN_HEIGHT) drawEnd = SCREEN_HEIGHT - 1;
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Red walls
+        SDL_RenderDrawLine(renderer, x, drawStart, x, drawEnd);
     }
+
+    SDL_RenderPresent(renderer);
 }
 
-void moveSmoothly(int newX, int newY) {
-    while (playerX != newX || playerY != newY) {
-        if (playerX < newX) playerX++;
-        if (playerX > newX) playerX--;
-        if (playerY < newY) playerY++;
-        if (playerY > newY) playerY--;
+int WinMain(int argc, char* argv[]) {
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
 
-        displayMaze();
-        Sleep(100);  
-    }
-}
+    if (initSDL(&window, &renderer) != 0) return 1;
 
-void movePlayer(char direction) {
-    int newX = playerX, newY = playerY;
+    int quit = 0;
+    SDL_Event e;
 
-    switch (direction) {
-        case 'w': newY--; break;
-        case 's': newY++; break;
-        case 'a': newX--; break;
-        case 'd': newX++; break;
-        default: return;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) quit = 1;
+        }
+
+        render3D(renderer);
+        SDL_Delay(16);
     }
 
-    if (maze[newY][newX] == ' ') {
-        moveSmoothly(newX, newY);  
-    }
-}
-
-int main() {
-    char input;
-    
-    while (1) {
-        displayMaze();
-        input = _getch();
-
-        if (input == 'q') break;  
-        movePlayer(input);
-    }
-
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
